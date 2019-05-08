ca60c261c39867ce06e8aa906d143a78a4e26fff
tianyang.yu <tianyang.yu@leyantech.com>
2019-05-08 18:02:42 +0800

    add

--- src/main/java/com/yutianyang/eureka/server/Application.java
+++ src/main/java/com/yutianyang/eureka/server/Application.java
@@ -1,10 +1,8 @@
 package com.yutianyang.eureka.server;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 
 @EnableEurekaServer
 @SpringBootApplication
